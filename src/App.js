96f79f167267fc8f97b882090d66794c40e0657e
Gabriel Coronel <gabriel.c1901@gmail.com>
2023-07-31 23:33:29 -0300

    add redux and fake data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Header from "./components/Header";
+import Center from "./components/Center";
 
 function App() {
   return (
     <div className="App">
+      <Header />
+      {/* <Center /> */}
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </div>
   );
